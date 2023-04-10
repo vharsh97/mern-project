042ebe2e09fada442fdb9bed399e7e91470a99e4
vharsh-srivastava <vharsh.srivastava@gmail.com>
2023-04-10 22:22:41 +0530

    Initial Commit. Defined index.js with all requied dependencies and logic to start the server

--- backend/config/config.js
+++ backend/config/config.js
@@ -5,7 +5,7 @@ dotenv.config({ path: path.join(__dirname, '../../.env') });
 
 module.exports = {
   server: {
-    port: process.env.PORT || 3000,
+    port: process.env.PORT || 8082,
   },
   db: {
     uri: process.env.MONGODB_URI || "mongodb://localhost:27017/users",
